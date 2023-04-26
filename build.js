7100dcabbbcf873ffc5026cb769b5a4d511159d0
thinhmascot <67301649+thinhmascot@users.noreply.github.com>
2023-04-26 17:05:40 +0700

    455

--- build.js
+++ build.js
@@ -3,11 +3,17 @@ import { build } from "esbuild";
 build({
   bundle: true,
   format: "esm",
-  mainFields: ["browser", "module", "main"],
-  platform: "neutral",
   target: "es2020",
-  entryPoints: ["./src/index.ts"],
-  outfile: "./dist/worker.mjs",
+  mainFields: ["browser", "module", "main"],
+  
+  //platform: "neutral",
+  platform: "node",
+  
+  
+  //entryPoints: ["./src/index.ts"],
+  entryPoints: ["./src/test.ts"],
+  
+  outfile: "./dist/index.js",
   sourcemap: false,
   charset: "utf8",
   minify: process.env.NODE_ENV === "production" ? true : false
